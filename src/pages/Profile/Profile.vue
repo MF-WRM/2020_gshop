a61b33c32950c3acfe32fc1bb6387694290eda1d
MF-WRM <wangwrm@163.com>
2020-12-29 23:04:49 +0800

    呃呃呃

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -13,7 +13,7 @@
             <i class="iconfont icon-person"></i>
           </div>
           <div class="user-info">
-            <p class="user-info-top">登录/注册</p>
+            <p class="user-info-top">登录/注册111</p>
             <p>
               <span class="user-icon">
                 <i class="iconfont icon-shouji icon-mobile"></i>
